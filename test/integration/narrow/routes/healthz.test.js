e5be9e4c6d11bc556a49ee32fb40e1d9973175e9
John Watson <john.watson1@defra.gov.uk>
2023-11-26 16:24:18 +0000

    Update tests

--- test/integration/narrow/routes/healthz.test.js
+++ test/integration/narrow/routes/healthz.test.js
@@ -1,21 +1,24 @@
-describe('Healthz test', () => {
-  const server = require('../../../../app/server')
+const { createServer } = require('../../../../app/server')
 
-  beforeEach(async () => {
-    await server.start()
-  })
+let server
 
+beforeEach(async () => {
+  server = await createServer()
+  await server.start()
+})
+
+afterEach(async () => {
+  await server.stop()
+})
+
+describe('Healthy test', () => {
   test('GET /healthz route returns 200', async () => {
     const options = {
       method: 'GET',
-      url: '/healthz'
+      url: '/auth/healthz'
     }
 
     const response = await server.inject(options)
     expect(response.statusCode).toBe(200)
-  })
-
-  afterEach(async () => {
-    await server.stop()
   })
 })
