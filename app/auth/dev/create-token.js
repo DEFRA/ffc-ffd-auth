0133ae490ff78712e4e42fa37bcb2476f54c8002
John Watson <john.watson1@defra.gov.uk>
2023-12-07 11:08:07 +0000

    Remove redundant log

--- app/auth/dev/create-token.js
+++ app/auth/dev/create-token.js
@@ -6,7 +6,6 @@ const { getKeys } = require('./get-keys')
 
 const createToken = async (crn) => {
   const { privateKey } = await getKeys()
-  console.log('Private key', privateKey)
   return jwt.sign({
     nonce: 'defaultNonce',
     contactId: crn,
