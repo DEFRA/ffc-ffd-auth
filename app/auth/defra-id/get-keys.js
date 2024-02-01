6d677dfca8ab8922b776f6de9eab42e0a8c35432
John Watson <john.watson1@defra.gov.uk>
2024-02-01 09:27:11 +0000

    Add temp logs

--- app/auth/defra-id/get-keys.js
+++ app/auth/defra-id/get-keys.js
@@ -4,12 +4,10 @@ const { getWellKnown } = require('./get-well-known')
 
 const getKeys = async () => {
   const { jwks_uri: url } = await getWellKnown()
-  console.log(`Getting keys from ${url}`)
   const { payload } = await Wreck.get(url, {
     json: true
   })
-  console.log('Got keys')
-  console.log(payload)
+
   const { keys } = payload
   const pem = jwkToPem(keys[0])
   return { publicKey: pem }
