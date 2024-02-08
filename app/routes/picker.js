59c0f8f48e568474339b26d5524c51906979eaee
Simon Dunn <simondunn90@gmail.com>
2024-02-08 13:31:51 +0000

    rm comments

--- app/routes/picker.js
+++ app/routes/picker.js
@@ -1,8 +1,5 @@
 const { GET } = require('../constants/http-verbs')
 const { reselectOrganisation } = require('../auth/defra-id/reselect-org')
-// const { AUTH_COOKIE_NAME, AUTH_REFRESH_COOKIE_NAME } = require('../constants/cookies')
-// const { authConfig } = require('../config')
-// const { getAccessToken, getAuthorizationUrl, getRedirectPath } = require('../auth')
 
 module.exports = [{
   method: GET,
@@ -11,14 +8,5 @@ module.exports = [{
     console.log('hello from picker')
     const redirect = request.query.redirect ?? '/landing-page/home'
     return h.redirect(await reselectOrganisation(redirect))
-
-    // if (request.auth.isAuthenticated) {
-    // }
-
-    // if (authConfig.defraIdEnabled) {
-    //   return h.redirect(await getAuthorizationUrl(redirect))
-    // }
-
-    // return h.view('sign-in', { redirect })
   }
 }]
