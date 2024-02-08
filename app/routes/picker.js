53223828be6d414455119c17963cf3a86dd1d3ad
Simon Dunn <simondunn90@gmail.com>
2024-02-08 13:37:35 +0000

    tidy up

--- app/routes/picker.js
+++ app/routes/picker.js
@@ -1,11 +1,10 @@
 const { GET } = require('../constants/http-verbs')
-const { reselectOrganisation } = require('../auth/defra-id/reselect-org')
+const { reselectOrganisation } = require('../auth/defra-id/reselect-organisation')
 
 module.exports = [{
   method: GET,
   path: '/picker',
   handler: async (request, h) => {
-    console.log('hello from picker')
     const redirect = request.query.redirect ?? '/landing-page/home'
     return h.redirect(await reselectOrganisation(redirect))
   }
