0487b0f13cc72226e2fd2c392ffb222011fb5d7d
John Watson <john.watson1@defra.gov.uk>
2024-02-19 17:32:01 +0000

    Add state verification to sign out

--- app/routes/sign-out-oidc.js
+++ app/routes/sign-out-oidc.js
@@ -1,12 +1,15 @@
 const { GET } = require('../constants/http-verbs')
 const { AUTH_COOKIE_NAME } = require('../constants/cookies')
 const { authConfig } = require('../config')
-const { decodeState, getRedirectPath, clearCache } = require('../auth')
+const { decodeState, getRedirectPath, clearCache, validateState } = require('../auth')
 
 module.exports = [{
   method: GET,
   path: '/sign-out-oidc',
   handler: async (request, h) => {
+    if (authConfig.defraIdEnabled) {
+      validateState(request, request.query.state)
+    }
     const state = decodeState(request.query.state)
     const redirect = getRedirectPath(state.redirect)
 
